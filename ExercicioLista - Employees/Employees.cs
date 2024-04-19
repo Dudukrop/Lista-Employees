2a735d77292345022fd93647f3875fd64d8165a3
Eduardo Kropniczki <dudukrop@gmail.com>
2024-04-19 17:36:58 -0300

    first commit

---
+++ ExercicioLista - Employees/Employees.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioLista___Employees
+{
+    internal class Employees
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+                
+        public Employees(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void salaryIncrease(double percent)
+        {
+            Salary += Salary * percent/100;
+        }
+
+        public override string ToString()
+        {
+            return Id
+            + ", "
+            + Name
+            + ", "
+            + Salary;
+        }
+    }
+}
---
+++ ExercicioLista - Employees/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+using System.Collections.Generic;
+using ExercicioLista___Employees;
+using System.Security.Cryptography.X509Certificates;
+
+Console.Write("How many employees will be registered? ");
+int n = int.Parse(Console.ReadLine());
+
+List<Employees> lista = new List<Employees>();
+
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine("Employee: #" + i);
+    Console.Write("Id: ");
+    int id = int.Parse(Console.ReadLine());
+    Console.Write("Name: ");
+    string name = Console.ReadLine();
+    Console.Write("Salary: ");
+    double salary = double.Parse(Console.ReadLine());
+
+    lista.Add(new Employees(id, name, salary));
+    Console.WriteLine();
+}
+
+Console.Write("Enter the employee id that will have salary increase : ");
+int findId = int.Parse(Console.ReadLine());
+
+Employees search = lista.Find(x => x.Id == findId);
+
+if (search != null)
+{
+    Console.Write("Enter the percentage: ");
+    double percentage = double.Parse(Console.ReadLine());
+    search.salaryIncrease(percentage);
+}
+else
+{
+    Console.WriteLine("This id does not exist!");
+}
+
+Console.WriteLine();
+Console.WriteLine("Updated list of employees:");
+foreach (Employees obj in lista)
+{
+    Console.WriteLine(obj);
+}
